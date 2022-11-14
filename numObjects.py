236f94ef07fb53dccc681dafafb4bd7b297bb53e
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2022-11-14 17:29:23 -0600

    Changed kwargs in numObjects

--- numObjects.py
+++ numObjects.py
@@ -1,15 +1,11 @@
 from tkinter import simpledialog
 
 
-
 def askNumObjects():
     numObjects = simpledialog.askinteger(
-        "Physics Sumulation",
-        "How many objects would you like to initialize?",
+        title="Physics Sumulation",
+        prompt="How many objects would you like to initialize?",
         minvalue=0,
-        maxvalue=10
+        maxvalue=10,
     )
     return numObjects
-
-
-
