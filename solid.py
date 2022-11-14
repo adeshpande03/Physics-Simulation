635bc9efc2c4e1e7587c6612dc3ca59feab8c1dc
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2022-11-14 10:18:24 -0600

    Added mass to the solid class

--- solid.py
+++ solid.py
@@ -1,5 +1,20 @@
-class Solid():
-    def __init__(self, x = 0, y = 0, leng = 1, wid = 1, xVel = 0, yVel = 0, xAccel = 0, yAccel = 0, tang = True, fric = 0, sticky = False, rigid = False):
+class Solid:
+    def __init__(
+        self,
+        x=0,
+        y=0,
+        leng=1,
+        wid=1,
+        xVel=0,
+        yVel=0,
+        xAccel=0,
+        yAccel=0,
+        tang=True,
+        fric=0,
+        mass=0,
+        sticky=False,
+        rigid=False,
+    ):
         self.x = x
         self.y = y
         self.leng = leng
@@ -10,7 +25,6 @@ class Solid():
         self.yAccel = yAccel
         self.tang = tang
         self.fric = fric
+        self.mass = mass
         self.sticky = sticky
         self.rigid = rigid
-    
-        
