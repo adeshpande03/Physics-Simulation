49ed96092162296f16d61767ef3d78e9af3f59a3
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2022-11-14 04:19:33 -0600

    Added run.py, main.py,

---
+++ main.py
@@ -0,0 +1,10 @@
+from run import *
+
+
+def main():
+    run()
+    print("Complete!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ run.py
@@ -0,0 +1,20 @@
+import pygame
+import math
+import pymunk
+import particle
+
+
+def run(length=960, width=594, fps=60, color=(0, 0, 0)):
+    pygame.init()
+    win = pygame.display.set_mode((length, width))
+    win.fill(color)
+    running = True
+    clock = pygame.time.Clock()
+
+    while running:
+        for instance in pygame.event.get():
+            if instance.type == pygame.QUIT:
+                running = False
+                break
+        pygame.display.update()
+        clock.tick(fps)
