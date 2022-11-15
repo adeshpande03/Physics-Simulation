d21907ce7b55885657423221d21fa4de9892ba2d
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2022-11-14 18:00:07 -0600

    Changed optional params in numObjects

--- numObjects.py
+++ numObjects.py
@@ -1,11 +1,11 @@
 from tkinter import simpledialog
 
 
-def askNumObjects():
+def askNumObjects(minvalue = 0, maxvalue = 10):
     numObjects = simpledialog.askinteger(
         title="Physics Sumulation",
         prompt="How many objects would you like to initialize?",
-        minvalue=0,
-        maxvalue=10,
+        minvalue = minvalue,
+        maxvalue = maxvalue,
     )
     return numObjects
