dd1bdfd86008af79938aacd8cd39e0eec360fa9e
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-11-14 21:06:32 -0600

    created checkSpace, modified collectdata

--- collectData.py
+++ collectData.py
@@ -11,7 +11,7 @@ def test():
 def collectData():
     dataDict = dict()
     numForms = numObjects.askNumObjects()
-    for _ in range(numForms):
+    while len(dataDict) < numForms:
         formData = createForm.createForm()
         dataDict[id(formData)] = formData
     return dataDict
